211c78cc3022722dab9fbbebe89054de9b100b52
tmjoris <tangajoris@gmail.com>
2025-07-07 19:22:48 +0300

    created the media model

---
+++ rust-core/src/models/media.rs
@@ -0,0 +1,25 @@
+use chrono;
+
+#[derive(Debug)]
+struct media {
+    pub sha1: [&u8];
+    let ts = chrono::offset::Utc::now();
+    pub remote_id: String;
+    pub local_reference: String;
+    pub local_size: i32;
+    pub key: [&u8];
+    pub meta_data [&u8];
+    pub type: i32;
+    pub state: i32;
+    let updated_at = chrono::offset::Utc::now();
+    pub file_name: String;
+    pub sort_order: i32;
+    pub error_count: i32;
+    pub version: i32;
+    pub format: i32;
+    pub error_message: String;
+    pub local_bucket: String;
+    pub liked: i32;
+    pub hidden: i32;
+    pub dirty: i32;
+}
